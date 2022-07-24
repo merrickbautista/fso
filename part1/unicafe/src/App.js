e369d8e5de3a4e3252a4d551041e142675a06db6
Merrick Bautista <merrickbautista@gmail.com>
2022-07-24 15:37:09 -0500

    Redid 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -2,7 +2,18 @@ import { useState } from 'react'
 
 const Header = ({text}) => <h1>{text}</h1>
 
-const Statistics = ({text, number, extra=''}) => <div>{text} {number} {extra}</div>
+const Stat = ({text, number, extra=''}) => <div>{text} {number} {extra}</div> 
+
+const Statistics = ({good,neutral,bad}) => (
+  <div>
+    <Stat text='good' number={good} />
+    <Stat text='neutral' number={neutral} />
+    <Stat text='bad' number={bad} />
+    <Stat text='all' number={good+neutral+bad} />
+    <Stat text='average' number={(good-bad)/(good+neutral+bad)} />
+    <Stat text='positive' number={good*100/(good+neutral+bad)} extra='%' />
+  </div>
+)
 
 const Button = ({onClick, text}) => (
   <button onClick={onClick}>
@@ -32,12 +43,7 @@ const App = () => {
         text='bad'
       />
       <Header text='statistics' />
-      <Statistics text='good' number={good} />
-      <Statistics text='neutral' number={neutral} />
-      <Statistics text='bad' number={bad} />
-      <Statistics text='all' number={good+neutral+bad} />
-      <Statistics text='average' number={(good-bad)/(good+neutral+bad)} />
-      <Statistics text='positive' number={good*100/(good+neutral+bad)} extra='%'/> 
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
