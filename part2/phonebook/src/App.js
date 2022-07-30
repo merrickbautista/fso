c6fd371b20ed060d69392319daed828b45273703
Merrick Bautista <merrickbautista@gmail.com>
2022-07-30 13:55:00 -0500

    Finished 2.7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,14 +10,27 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const checkExisting = (name) => {
+    for (let i = 0; i < persons.length; i++) {
+      if (JSON.stringify(persons[i].name) === JSON.stringify(name)) {
+        return true
+      }
+    }
+    return false
+  }
+
   const addPerson = (event) => {
     event.preventDefault()
-    const personObject = {
-      name: newName,
-      id: persons.length + 1
+    if (!checkExisting(newName)) {
+      const personObject = {
+        name: newName,
+        id: persons.length + 1
+      }
+      setPersons(persons.concat(personObject))
     }
-
-    setPersons(persons.concat(personObject))
+    else {
+      alert(`${newName} is already added to phonebook`)
+    }
     setNewName('')
   }
 
