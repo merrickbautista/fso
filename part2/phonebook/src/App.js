cc173844f0a75f1e647ef277504136148bae9f4d
Merrick Bautista <merrickbautista@gmail.com>
2022-07-30 15:01:36 -0500

    Finished 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -13,6 +17,11 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+    setPersons(persons.filter(person => person.name.includes(event.target.value)))
   }
 
   const checkExisting = (name) => {
@@ -29,7 +38,8 @@ const App = () => {
     if (!checkExisting(newName)) {
       const personObject = {
         name: newName,
-        number: newNumber
+        number: newNumber,
+        id: persons.length + 1
       }
       setPersons(persons.concat(personObject))
     }
@@ -43,6 +53,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      filter shown with <input
+        value={newFilter}
+        onChange={handleFilterChange}
+      />
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
