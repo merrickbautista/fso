c8220cdbf67a249b5c79c465cb6b9f6a0488525d
Merrick Bautista <merrickbautista@gmail.com>
2022-08-25 17:59:29 -0500

    Finished 2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -10,7 +10,23 @@ const Languages = ({languages}) => {
   )
 }
 
-const Countries = ({filtered}) => {
+const Country = ({country}) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>
+        capital {country.capital}
+        <br />
+        area {country.area}
+      </p>
+      <h3>languages: </h3>
+      <Languages languages={country.languages} />
+      <img src={country.flags.png} />
+    </div>
+  )
+}
+
+const Countries = ({filtered, setFiltered}) => {
   if (filtered.length > 10) {
     return (
       <div>
@@ -19,24 +35,16 @@ const Countries = ({filtered}) => {
     )
   }
   else if (filtered.length == 1) {
-    let country = filtered[0]
     return (
-      <div>
-        <h1>{country.name.common}</h1>
-        <p>
-          capital {country.capital}
-          <br />
-          area {country.area}
-        </p>
-        <h3>languages: </h3>
-        <Languages languages={country.languages} />
-        <img src={country.flags.png} />
-      </div>
+      <Country country={filtered[0]} />
     )
   }
   return (
     <div>
-      {filtered.map(country => <div key={country.name.common}>{country.name.common}</div>)}
+      {filtered.map(country => <div key={country.name.common}>
+        {country.name.common}
+        <button onClick={() => setFiltered([country])}>show</button>
+      </div>)}
     </div>
   )
 }
@@ -65,7 +73,7 @@ useEffect(() => {
   return (
     <div>
       find countries <input onChange={handleFilterChange} />
-      <Countries filtered={filtered} />
+      <Countries filtered={filtered} setFiltered={setFiltered} />
     </div>
   )
 }
